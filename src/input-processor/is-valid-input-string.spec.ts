669d93799206408a930d0836f4a9bcad6ff4decd
Md Kamrul Islam <kamruli@gmail.com>
2019-11-09 22:58:30 +1100

    added more test

--- src/input-processor/is-valid-input-string.spec.ts
+++ src/input-processor/is-valid-input-string.spec.ts
@@ -13,3 +13,21 @@ test('isValidInputString test :place', () => {
     expect(isValidInputString('place 3,4, north')).toBe(true);
     expect(isValidInputString('place 03, 04, north')).toBe(true);
 });
+
+test('isValidInputString test :report', () => {
+    expect(isValidInputString('report')).toBe(true);
+    expect(isValidInputString('report 3,4,5')).toBe(false);
+    expect(isValidInputString('REPORT')).toBe(true);
+});
+
+test('isValidInputString test :left', () => {
+    expect(isValidInputString('LEFT')).toBe(true);
+    expect(isValidInputString('left')).toBe(true);
+    expect(isValidInputString('left 3')).toBe(false);
+});
+
+test('isValidInputString test :right', () => {
+    expect(isValidInputString('RIGHT')).toBe(true);
+    expect(isValidInputString('right')).toBe(true);
+    expect(isValidInputString('right 3')).toBe(false);
+});
