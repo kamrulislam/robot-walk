3ad775253cd4e4904ce94881ab4356cb663944cc
Md Kamrul Islam <kamruli@gmail.com>
2019-11-09 14:16:02 +1100

    updated debug message

--- src/log/index.ts
+++ src/log/index.ts
@@ -1,6 +1,11 @@
 
 export const log = (...args: Array<any>) => {
     if (process.env.DEBUG) {
+        if (typeof args[0] === 'string') {
+            args[0] = `[DEBUG]::${args[0]}`
+        } else {
+            args = ['[DEBUG]::', ...args];
+        }
         console.log(...args);
     }
 }
