d796dba089bf8736559821487293497c657cdc5f
Md Kamrul Islam <kamruli@gmail.com>
2019-11-10 13:44:56 +1100

    added hint link

--- src/to.ts
+++ src/to.ts
@@ -1,3 +1,4 @@
+// inspired by https://blog.grossman.io/how-to-write-async-await-without-try-catch-blocks-in-javascript/
 
 export default (promise: Promise<any>):Promise<any[]> => {
     return promise.then((data: any) => {
