b62831b4c7387653b1a0089d3f8a4a15bf080efe
Md Kamrul Islam <kamruli@gmail.com>
2019-11-10 18:02:52 +1100

    updated more test cases

--- src/command-processor/process-command.spec.ts
+++ src/command-processor/process-command.spec.ts
@@ -19,4 +19,79 @@ describe('ProcessCommand', () => {
 
         expect(placeInformation).toBeDefined();
     });
+
+    test('test left turn command', () => {
+        let placeInformation = processCommand.turn(CommandType.LEFT, undefined);
+
+        expect(placeInformation).toBeUndefined();
+        placeInformation = processCommand.place({
+            commandType: CommandType.PLACE,
+            commandParams: {
+                posX: 0,
+                posY: 0,
+                direction: DirectionType.NORTH
+            }
+        }, undefined);
+
+        placeInformation = processCommand.turn(CommandType.LEFT, placeInformation);
+        expect(placeInformation).toEqual({
+            ...placeInformation,
+            direction: DirectionType.WEST
+        });
+
+    });
+
+    test('test right turn command', () => {
+        let placeInformation = processCommand.turn(CommandType.RIGHT, undefined);
+
+        expect(placeInformation).toBeUndefined();
+        placeInformation = processCommand.place({
+            commandType: CommandType.PLACE,
+            commandParams: {
+                posX: 0,
+                posY: 0,
+                direction: DirectionType.NORTH
+            }
+        }, undefined);
+
+        placeInformation = processCommand.turn(CommandType.RIGHT, placeInformation);
+        expect(placeInformation).toEqual({
+            ...placeInformation,
+            direction: DirectionType.EAST
+        });
+
+    });
+
+    test('test Move command', () => {
+        let placeInformation = processCommand.turn(CommandType.RIGHT, undefined);
+
+        expect(placeInformation).toBeUndefined();
+        placeInformation = processCommand.place({
+            commandType: CommandType.PLACE,
+            commandParams: {
+                posX: 0,
+                posY: 0,
+                direction: DirectionType.NORTH
+            }
+        }, undefined);
+
+        placeInformation = processCommand.move(placeInformation);
+        expect(placeInformation).toEqual({
+            ...placeInformation,
+            posY: 1
+        });
+
+        placeInformation = processCommand.move(placeInformation);
+        expect(placeInformation).toEqual({
+            ...placeInformation,
+            posY: 2
+        });
+
+        placeInformation = processCommand.turn(CommandType.RIGHT, placeInformation);
+        expect(placeInformation).toEqual({
+            ...placeInformation,
+            direction: DirectionType.EAST
+        });
+
+    });
 });
